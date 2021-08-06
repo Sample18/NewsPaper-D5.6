3d55194bd9e3ebc3dec93c267e700390a29a70a8
Sample18 <78348245+Sample18@users.noreply.github.com>
2021-08-06 21:36:42 +0300

    Update settings.py
    /

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -167,7 +167,7 @@ ACCOUNT_EMAIL_VERIFICATION = True
 EMAIL_HOST = 'smtp.yandex.ru' # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465 # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'sample417' # ваше имя пользователя, например если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'Sample180824S417087530' # пароль от почты
+EMAIL_HOST_PASSWORD = '' # пароль от почты
 EMAIL_USE_SSL = True # Яндекс использует ssl, подробнее о том, что это, почитайте на Википедии, но включать его здесь обязательно
 
 
